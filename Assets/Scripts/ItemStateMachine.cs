5be03b1d042fd6be0311a7e6ff54199acb1b144d
truongpx08 <truongpx.08081998@gmail.com>
2024-09-05 15:34:06 +0700

    Items: Add space between 2 items when in the same line

--- Assets/Scripts/ItemStateMachine.cs
+++ Assets/Scripts/ItemStateMachine.cs
@@ -65,11 +65,28 @@ public class ItemBaseState : MonoBehaviour
 
 public class AppearingState : ItemBaseState, IEnterState
 {
+    private const float SpacingX = 4;
+    public const float LeftLimit = -6f; // Giới hạn bên trái  
+    public const float RightLimit = 6f; // Giới hạn bên phải  
+
     public void Enter()
     {
         LoadItemReference();
         this.itemReference.SR.sprite = this.itemReference.CharacterSprite;
-        itemReference.transform.localPosition = new Vector3(Random.Range(-3, 3), 0, 0);
+
+        //Set Position
+        var line = this.itemReference.transform.parent;
+        if (line.childCount == 2)
+        {
+            var other = line.GetChild(0);
+            var otherX = other.transform.localPosition.x;
+            itemReference.transform.localPosition = otherX > 0
+                ? new Vector3(Random.Range(LeftLimit, Mathf.Min(0f, otherX - SpacingX)), 0, 0)
+                : new Vector3(Random.Range(Mathf.Max(0f, otherX + SpacingX), RightLimit), 0, 0);
+        }
+        else
+            itemReference.transform.localPosition = new Vector3(Random.Range(LeftLimit, RightLimit), 0, 0);
+
         this.itemReference.StateMachine.ChangeState(EItemState.Moving);
     }
 }
@@ -77,12 +94,11 @@ public class AppearingState : ItemBaseState, IEnterState
 public class ItemMovingState : ItemBaseState, IEnterState, IExitState
 {
     [SerializeField] private bool entering;
-    [SerializeField] private float speed = 2f; // Tốc độ di chuyển  
-    [SerializeField] private float leftLimit = -3f; // Giới hạn bên trái  
-    [SerializeField] private float rightLimit = 3f; // Giới hạn bên phải  
+    [SerializeField] private float speed = 4f; // Tốc độ di chuyển  
+
     [SerializeField] private bool movingToRight;
     private Vector3 targetPosition; // Vị trí đích  
-    private Vector3 ItemPosition => this.itemReference.transform.position;
+    private Vector3 ItemPosition => this.itemReference.transform.localPosition;
 
     public void Enter()
     {
@@ -115,7 +131,7 @@ public class ItemMovingState : ItemBaseState, IEnterState, IExitState
     private void Move()
     {
         // Di chuyển vật phẩm tới vị trí mục tiêu  
-        this.itemReference.transform.position =
+        this.itemReference.transform.localPosition =
             Vector3.MoveTowards(ItemPosition, targetPosition, speed * UnityEngine.Time.deltaTime);
 
         // Kiểm tra nếu vật phẩm đã đến vị trí mục tiêu  
@@ -144,8 +160,8 @@ public class ItemMovingState : ItemBaseState, IEnterState, IExitState
     private void SetTargetPosition()
     {
         targetPosition = movingToRight
-            ? new Vector3(rightLimit, ItemPosition.y, ItemPosition.z)
-            : new Vector3(leftLimit, ItemPosition.y, ItemPosition.z);
+            ? new Vector3(AppearingState.RightLimit, ItemPosition.y, ItemPosition.z)
+            : new Vector3(AppearingState.LeftLimit, ItemPosition.y, ItemPosition.z);
     }
 }
 
